1379fdc59815611b2f343f301b67798d66c7a385
abicoto <a.coto.5@estudiantec.cr>
2026-04-24 18:55:24 -0600

    agregue el código

---
+++ TP1.py
@@ -0,0 +1,37 @@
+def mostrarMenu():
+    print ("Menú\n")
+    print ("1. Cargar Tokens")
+    print ("2. Mostrar tokens")
+    print ("3. Agregar/modificar token")
+    print ("4. Guardar tokens")
+    print ("5. Traducir código")
+    print ("6. Generar CSV")
+    print ("7. Generar HTML")
+    print ("8. Submenú de bitácora")
+    print ("9. Salir\n")
+
+opcion = ""
+while opcion != "9":
+    mostrarMenu()
+    opcion = input ("Elija una opción: ")
+
+    if opcion == "1":
+        print("Cargar Tokens")
+    elif opcion == "2":
+        print("Mostrar tokens")
+    elif opcion == "3":
+        print("Agregar/modificar tokens")
+    elif opcion == "4":
+        print("Guardar tokens")
+    elif opcion == "5":
+        print("Traducir código")
+    elif opcion == "6":
+        print("Generar CSV")
+    elif opcion == "7":
+        print("Generar HTML")
+    elif opcion == "8":
+        print("Elija una opción\nA)Acciones por día recogido\nB)Acciones con algunas palabras clave\nC)Salir del submenú")
+    elif opcion == "9":
+        print("Ha salido con éxito") 
+    else:
+        print("Opción no valida") 
